8d526c48df551bc20a9dae6c2ba04ca4445e805c
amandinemeeus <amandine.meeus@alumni.ulb.be>
2017-11-28 12:17:14 +0100

    ébauche

---
+++ index.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>AllezCine</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/reset.css"/>
+    <link rel="stylesheet" href="./assets/css/main.css"/>
+
+  </head>
+
+  <body id="fr">
+	<!-- Main Start -->
+	<div class="main">
+
+    <!-- Main Header Start -->
+        <header class="main-header">
+			<div class="centered">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-lg-3 logo">
+                			<a href="index.html"><h1>LOGO<span>Movies</span></h1></a>
+                            <img src="assets/images/1.jpg" class="logo-img" alt="">
+                		</div>
+                		<div class="col-12 col-lg-6 searchbar pl-2">
+                			<form action="#" method="post">
+                				<input type="text" name="recherche" placeholder="Rechercher" required="">
+                				<input type="submit" value="Go">
+                			</form>
+                		</div>
+                		<div class="col-12 col-lg-3 login pl-5">
+                			<ul>
+                				<li><i class="fa fa-phone" aria-hidden="true"></i> (000) 123 456 789</li>
+                				<li><a href="#" data-toggle="" data-target="">Login</a></li>
+                			</ul>
+                		</div>
+                      </div>
+                   </div>
+            </div>
+        </header>
+	<!-- Main Header End -->
+
+    <!-- Nav start-->
+
+    <!-- nav -->
+    <!-- nav -->
+    	<div class="movies_nav">
+    		<div class="container-fluid">
+                <div class="col-lg-3">
+
+                </div>
+                <div class="col-lg-6">
+
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown link
+</a>
+<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+<a class="dropdown-item" href="#">Action</a>
+<a class="dropdown-item" href="#">Another action</a>
+<a class="dropdown-item" href="#">Something else here</a>
+</div>
+</li>
+    </ul>
+  </div>
+  </div>
+</nav>
+<div class="col-lg-3">
+
+</div>
+    		</div>
+
+
+    	</div>
+    <!-- //nav -->
+
+    <!-- Nav end-->
+
+    <!-- Main Content Start -->
+		<section class="main-content" id="main-content">
+
+            <!-- Content Header Start -->
+            <div class="content-header">
+				<div class="centered">
+                </div>
+            </div>
+            <!-- Content Header End -->
+
+            <!-- Content Content-inner Start -->
+			<div class="centered content-inner">
+				<div class="content" id="contact">
+                </div>
+            </div>
+			<!-- Content Content-inner End -->
+
+			<!-- Content Footer Start -->
+      		<footer class="content-footer">
+      			<div class="align-center">
+    			    <div class="copyrights">
+    			      © AllezCine.be 2017 - By  - <a href="" class="simple-link">Contact</a>
+    			    </div>
+    			</div>
+      		</footer>
+      		<!-- Content Footer End -->
+    </section>
+		<!-- Main Content End -->
+
+  </div>
+	<!-- Main End -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
